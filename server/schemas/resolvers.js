496005b9cbc56d0add8719158edb685d18ba931f
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-10 22:47:17 -0500

    create add user and login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 //resolvers serve the response for the queries on the typeDefs.js page
 const resolvers = {
@@ -7,22 +8,44 @@ const resolvers = {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
         },
-        thought: async (parent, {_id}) => {
-            return Thought.findOne({_id});
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
         },
         // get all users
         users: async () => {
             return User.find()
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
         },
         // get user by username
-        user: async (parent, {username}) => {
-            return User.findOne({username})
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+
+            return user;
+        },
+        login: async (parent, {email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return user;
         }
     }
 };
